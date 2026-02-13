3aabb94a18f44d2bbfc1d4e6b5b02dd17abae21d
Saumya111-21 <kashyapsaumya199@gmail.com>
2026-02-13 11:06:50 +0530

    Delete homework2.py

--- homework2.py
+++
@@ -1,9 +0,0 @@
-def union_arrays(a, b):
-    # Use set to store distinct elements
-    union_set = set(a) | set(b)
-    return list(union_set)
-
-# Example
-a = [1, 2, 3, 2, 1]
-b = [3, 2, 2, 3, 3, 2]
-print(union_arrays(a, b))  
